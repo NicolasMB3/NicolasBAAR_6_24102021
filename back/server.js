038fbd2252b0c187531461f966414218901f489b
Nicolas <76515190+NicolasMB3@users.noreply.github.com>
2021-11-03 14:03:59 +0100

    Mise en place du projet

---
+++ back/server.js
@@ -0,0 +1,49 @@
+const http = require('http');
+const app = require('./app');
+
+// Renvoie un port valide, qu'il soit founi sous la forme d'un numéro ou d'une chaîne (string)
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+// Recherche les différentes erreurs et les gères de manière appropriée et les enregistres dans le serveur
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
